1b2bdffbf9859c7ad1ef3d328d81f8f283a4f280
andreawork-hub <andrea.vitova@hotmail.com>
2023-01-17 15:26:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect();
+//mongoose.connect();
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('strictQuery', true);
 // create a write stream (in append mode), log.txt file is created in root directory
